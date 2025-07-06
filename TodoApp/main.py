726d66b4cf5fb2943e9e90b00829c14dee03663f
Nicola <nyc.zyp@gmail.com>
2025-07-06 22:42:40 +0100

    9.115 - Delete Request (Todo Project)

--- TodoApp/main.py
+++ TodoApp/main.py
@@ -34,6 +34,7 @@ class TodoRequest(BaseModel):
 async def read_all(db: db_dependency):
     return db.query(Todos).all()
 
+
 @app.get("/todo/{todo_id}", status_code=status.HTTP_200_OK)
 async def read_todo(db: db_dependency, todo_id: int = Path(gt=0)):
     todo_model = db.query(Todos).filter(Todos.id == todo_id).first()
@@ -41,11 +42,13 @@ async def read_todo(db: db_dependency, todo_id: int = Path(gt=0)):
         return todo_model
     raise HTTPException(status_code=404, detail="Todo not found")
 
+
 @app.post("/todo", status_code=status.HTTP_201_CREATED)
 async def create_todo(db: db_dependency, todo_request: TodoRequest):
     todo_model = Todos(**todo_request.model_dump())
     db.add(todo_model)
     db.commit()
+
 
 @app.put("/todo/{todo_id}", status_code=status.HTTP_204_NO_CONTENT)
 async def update_todo(
@@ -63,4 +66,17 @@ async def update_todo(
     todo_model.complete = todo_request.complete
     
     db.add(todo_model)
+    db.commit()
+
+
+@app.delete("/todo/{todo_id}", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_todo(
+    db: db_dependency,
+    todo_id: int = Path(gt=0),
+):
+    todo_model = db.query(Todos).filter(Todos.id == todo_id).first()
+    if todo_model is None:
+        raise HTTPException(status_code=404, detail="Todo not found")
+    db.query(Todos).filter(Todos.id == todo_id).delete()
+    
     db.commit()
