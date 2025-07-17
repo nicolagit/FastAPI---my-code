e4ca972ab391fc780eaba58ebd101d8f4ea38313
Nicola <nyc.zyp@gmail.com>
2025-07-17 21:54:16 +0100

    11.134 - Delete Todo (User ID)

--- TodoApp/routers/todos.py
+++ TodoApp/routers/todos.py
@@ -95,12 +95,15 @@ async def update_todo(
 
 @router.delete("/todo/{todo_id}", status_code=status.HTTP_204_NO_CONTENT)
 async def delete_todo(
+        user: user_dependency,
         db: db_dependency,
         todo_id: int = Path(gt=0),
 ):
-    todo_model = db.query(Todos).filter(Todos.id == todo_id).first()
+    if user is None:
+        raise HTTPException(status_code=401, detail="Authentication failed")
+    todo_model = db.query(Todos).filter(Todos.id == todo_id).filter(Todos.owner_id == user.get("id")).first()
     if todo_model is None:
         raise HTTPException(status_code=404, detail="Todo not found")
-    db.query(Todos).filter(Todos.id == todo_id).delete()
+    db.query(Todos).filter(Todos.id == todo_id).filter(Todos.owner_id == user.get("id")).delete()
 
     db.commit()
