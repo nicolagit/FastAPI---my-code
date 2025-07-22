a8595f0a90fa192a4ab71465f4d963ee10961ff3
Nicola <nyc.zyp@gmail.com>
2025-07-22 22:18:30 +0100

    12.148 - Connect FastAPI to MySQL

--- TodoApp/database.py
+++ TodoApp/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 
-SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:[removed_from_repository]@localhost/TodoApplicationDatabase'
+SQLALCHEMY_DATABASE_URL = 'mysql+pymysql://root:[removed_from_repository]@127.0.0.1:3306/todoapplicationdatabase'
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
