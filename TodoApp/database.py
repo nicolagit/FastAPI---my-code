6e8929b4118d68feaf3a26a00e2853bd21c891c2
Nicola <nyc.zyp@gmail.com>
2025-07-23 21:20:35 +0100

    revert to local sqlite3 database

--- TodoApp/database.py
+++ TodoApp/database.py
@@ -3,9 +3,13 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 
-SQLALCHEMY_DATABASE_URL = 'mysql+pymysql://root:[removed_from_repository]@127.0.0.1:3306/todoapplicationdatabase'
+SQLALCHEMY_DATABASE_URL = 'sqlite:///./todosapp.db'
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL,
+    connect_args={"check_same_thread": False},
+)
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
