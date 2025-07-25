0538b7a29281a4a67f9e83bb98607068f88a8695
Nicola <nyc.zyp@gmail.com>
2025-07-25 19:03:52 +0100

    Alembic Revision Upgrade

---
+++ TodoApp/alembic/versions/461447a35b19_create_phone_number_for_user_column.py
@@ -0,0 +1,28 @@
+"""Create phone number for user column
+
+Revision ID: 461447a35b19
+Revises: 
+Create Date: 2025-07-25 18:54:27.965201
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = '461447a35b19'
+down_revision: Union[str, Sequence[str], None] = None
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    """Upgrade schema."""
+    op.add_column('users', sa.Column('phone_number', sa.String(), nullable=True))
+
+
+def downgrade() -> None:
+    """Downgrade schema."""
+    pass
