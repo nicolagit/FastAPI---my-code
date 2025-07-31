e7885c53d1ec04903b8d346917b59436abae5c1f
Nicola <nyc.zyp@gmail.com>
2025-07-31 23:29:41 +0100

    14.165 - Root Package

--- TodoApp/main.py
+++ TodoApp/main.py
@@ -1,12 +1,12 @@
 from fastapi import FastAPI
-import models
-from database import engine
-from routers import auth, todos, admin, users
+from .models import Base
+from .database import engine
+from .routers import auth, todos, admin, users
 
 
 app = FastAPI()
 
-models.Base.metadata.create_all(bind=engine)
+Base.metadata.create_all(bind=engine)
 
 
 @app.get("/healthy")
