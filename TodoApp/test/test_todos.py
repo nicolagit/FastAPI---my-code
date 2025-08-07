99e51629a01fae751bd5a5ea41e0621453a5d32b
Nicola <nyc.zyp@gmail.com>
2025-08-07 13:14:45 +0100

    14.172 - Project Test Part 3

--- TodoApp/test/test_todos.py
+++ TodoApp/test/test_todos.py
@@ -84,10 +84,12 @@ def test_read_one_authenticated(test_todo):
         "owner_id": 1,
       }
 
+
 def test_read_one_authenticated_not_found(test_todo):
     response = client.get("/todo/999")
     assert response.status_code == status.HTTP_404_NOT_FOUND
     assert response.json() == {'detail': 'Todo not found'}
+
 
 def test_create_todo(test_todo):
     request_data = {
@@ -105,3 +107,47 @@ def test_create_todo(test_todo):
     assert model.description == request_data.get("description")
     assert model.priority == request_data.get("priority")
     assert model.complete == request_data.get("complete")
+
+
+def test_update_todo(test_todo):
+    request_data = {
+        'title': 'Change the title of the todo already saved!',
+        'description': 'Need to learn everyday!',
+        'priority': 5,
+        'complete': False,
+    }
+    
+    response = client.put("/todo/1", json=request_data)
+    assert response.status_code == status.HTTP_204_NO_CONTENT
+    db = TestingSessionLocal()
+    model = db.query(Todos).filter(Todos.id == 1).first()
+    assert model.title == 'Change the title of the todo already saved!'
+
+
+def test_update_todo_not_found(test_todo):
+    request_data = {
+        'title': 'Change the title of the todo already saved!',
+        'description': 'Need to learn everyday!',
+        'priority': 5,
+        'complete': False,
+    }
+    
+    response = client.put("/todo/999", json=request_data)
+    assert response.status_code == status.HTTP_404_NOT_FOUND
+    assert response.json() == {'detail': 'Todo not found'}
+
+
+def test_delete_todo(test_todo):
+    response = client.delete("/todo/1")
+    assert response.status_code == status.HTTP_204_NO_CONTENT
+    
+    db = TestingSessionLocal()
+    model = db.query(Todos).filter(Todos.id == 1).first()
+    assert model is None
+
+
+def test_delete_todo_not_found():
+    response = client.delete("/todo/999")
+    assert response.status_code == status.HTTP_404_NOT_FOUND
+    assert response.json() == {'detail': 'Todo not found'}
+    
