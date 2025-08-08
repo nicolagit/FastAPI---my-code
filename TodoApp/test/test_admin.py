f2ea9015028fa95e3870b2bc6676930b65db03b8
Nicola <nyc.zyp@gmail.com>
2025-08-08 11:43:29 +0100

    14.175 - Project Test Part 6

--- TodoApp/test/test_admin.py
+++ TodoApp/test/test_admin.py
@@ -1,9 +1,12 @@
 from .utils import *
 from ..routers.admin import get_db, get_current_user
 from fastapi import status
+from ..models import Todos
+
 
 app.dependency_overrides[get_db] = override_get_db
 app.dependency_overrides[get_current_user] = override_get_current_user
+
 
 def test_admin_read_all_authenticated(test_todo):
     response = client.get("/admin/todos")
@@ -17,3 +20,17 @@ def test_admin_read_all_authenticated(test_todo):
         "priority": 5,
         "owner_id": 1,
     }]
+
+
+def test_admin_delete_todo(test_todo):
+    response = client.delete("/admin/todo/1")
+    assert response.status_code == status.HTTP_204_NO_CONTENT
+
+    db = TestingSessionLocal()
+    model = db.query(Todos).filter(Todos.id == 1).first()
+    assert model is None
+
+
+def test_admin_delete_todo_not_found(test_todo):
+    response = client.delete("/admin/todo/999")
+    assert response.status_code == status.HTTP_404_NOT_FOUND
