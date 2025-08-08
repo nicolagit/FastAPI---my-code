c67000997b5fb67053925b7225d3d9fa92ebd84e
Nicola <nyc.zyp@gmail.com>
2025-08-08 12:23:10 +0100

    14.176 - Project Test Part 7

---
+++ TodoApp/test/test_users.py
@@ -0,0 +1,18 @@
+from .utils import *
+from ..routers.users import get_db, get_current_user
+from fastapi import status
+
+
+app.dependency_overrides[get_db] = override_get_db
+app.dependency_overrides[get_current_user] = override_get_current_user
+
+
+def test_return_user(test_user):
+    response = client.get("/user")
+    assert response.status_code == status.HTTP_200_OK
+    assert response.json()['username'] == 'codingwithrobytest'
+    assert response.json()['email'] == 'codingwithrobytest@email.com'
+    assert response.json()['first_name'] == 'Eric'
+    assert response.json()['last_name'] == 'Roby'
+    assert response.json()['role'] == 'admin'
+    assert response.json()['phone_number'] == '(111)-111-1111'
