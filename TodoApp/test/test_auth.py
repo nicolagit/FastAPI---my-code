4c8153805349977e707e9478a37bf25a01d17e08
Nicola <nyc.zyp@gmail.com>
2025-08-10 17:27:49 +0100

    14.179 - Project Test Part 10

--- TodoApp/test/test_auth.py
+++ TodoApp/test/test_auth.py
@@ -1,8 +1,11 @@
 from .utils import *
-from ..routers.auth import get_db, authenticate_user
+from ..routers.auth import get_db, authenticate_user, create_access_token, SECRET_KEY, ALGORITHM
+from jose import jwt
+from datetime import datetime, timedelta
 
 
 app.dependency_overrides[get_db] = override_get_db
+
 
 def test_authenticate_user(test_user):
     db = TestingSessionLocal()
@@ -16,3 +19,27 @@ def test_authenticate_user(test_user):
     
     wrong_password_user = authenticate_user(test_user.username, "wrongpassword", db)
     assert wrong_password_user is False
+
+
+def test_create_access_token():
+    username = 'testuser'
+    user_id = 1
+    role = 'user'
+    expires_delta = timedelta(days=1)
+    
+    token = create_access_token(
+        username,
+        user_id,
+        role,
+        expires_delta,
+    )
+    decoded_token = jwt.decode(
+        token,
+        SECRET_KEY,
+        algorithms=[ALGORITHM],
+        options={"verify_signature": False},
+    )
+    
+    assert decoded_token['sub'] == username
+    assert decoded_token['id'] == user_id
+    assert decoded_token['role'] == role
