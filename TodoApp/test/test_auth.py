95404c0ca6d74610ec5b4e8113664b13dd0b2e70
Nicola <nyc.zyp@gmail.com>
2025-08-10 17:37:19 +0100

    14.180 - Project Tets Part 11

--- TodoApp/test/test_auth.py
+++ TodoApp/test/test_auth.py
@@ -1,7 +1,8 @@
 from .utils import *
-from ..routers.auth import get_db, authenticate_user, create_access_token, SECRET_KEY, ALGORITHM
+from ..routers.auth import get_db, authenticate_user, create_access_token, SECRET_KEY, ALGORITHM, get_current_user
 from jose import jwt
-from datetime import datetime, timedelta
+from datetime import timedelta
+import pytest
 
 
 app.dependency_overrides[get_db] = override_get_db
@@ -43,3 +44,13 @@ def test_create_access_token():
     assert decoded_token['sub'] == username
     assert decoded_token['id'] == user_id
     assert decoded_token['role'] == role
+
+
+@pytest.mark.asyncio
+async def test_get_current_user_valid_token():
+    encode = {'sub': 'testuser', 'id': 1, 'role': 'admin'}
+    token = jwt.encode(encode, SECRET_KEY, algorithm=ALGORITHM)
+    
+    user = await get_current_user(token=token)
+
+    assert user == {'username': 'testuser', 'id': 1, 'user_role': 'admin'}
