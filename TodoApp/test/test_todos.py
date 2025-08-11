3e88bb950b4dd43c118b41f2180b8cfce27515ad
Nicola <nyc.zyp@gmail.com>
2025-08-11 20:05:34 +0100

    15.183 - Setup Application (Change Todos Tests)

--- TodoApp/test/test_todos.py
+++ TodoApp/test/test_todos.py
@@ -9,7 +9,7 @@ app.dependency_overrides[get_current_user] = override_get_current_user
 
 
 def test_read_all_authenticated(test_todo):
-    response = client.get("/")
+    response = client.get("/todos/")
     assert response.status_code == status.HTTP_200_OK
     assert response.json() == [{
         "complete": False,
@@ -22,7 +22,7 @@ def test_read_all_authenticated(test_todo):
 
 
 def test_read_one_authenticated(test_todo):
-    response = client.get("/todo/1")
+    response = client.get("/todos/todo/1")
     assert response.status_code == status.HTTP_200_OK
     assert response.json() == {
         "complete": False,
@@ -35,7 +35,7 @@ def test_read_one_authenticated(test_todo):
 
 
 def test_read_one_authenticated_not_found(test_todo):
-    response = client.get("/todo/999")
+    response = client.get("/todos/todo/999")
     assert response.status_code == status.HTTP_404_NOT_FOUND
     assert response.json() == {'detail': 'Todo not found'}
 
@@ -47,7 +47,7 @@ def test_create_todo(test_todo):
         "priority": 5,
         "complete": False,
     }
-    response = client.post("/todo/", json=request_data)
+    response = client.post("/todos/todo/", json=request_data)
     assert response.status_code == status.HTTP_201_CREATED
     
     db = TestingSessionLocal()
@@ -66,7 +66,7 @@ def test_update_todo(test_todo):
         'complete': False,
     }
     
-    response = client.put("/todo/1", json=request_data)
+    response = client.put("/todos/todo/1", json=request_data)
     assert response.status_code == status.HTTP_204_NO_CONTENT
     db = TestingSessionLocal()
     model = db.query(Todos).filter(Todos.id == 1).first()
@@ -81,13 +81,13 @@ def test_update_todo_not_found(test_todo):
         'complete': False,
     }
     
-    response = client.put("/todo/999", json=request_data)
+    response = client.put("/todos/todo/999", json=request_data)
     assert response.status_code == status.HTTP_404_NOT_FOUND
     assert response.json() == {'detail': 'Todo not found'}
 
 
 def test_delete_todo(test_todo):
-    response = client.delete("/todo/1")
+    response = client.delete("/todos/todo/1")
     assert response.status_code == status.HTTP_204_NO_CONTENT
     
     db = TestingSessionLocal()
@@ -96,7 +96,7 @@ def test_delete_todo(test_todo):
 
 
 def test_delete_todo_not_found():
-    response = client.delete("/todo/999")
+    response = client.delete("/todos/todo/999")
     assert response.status_code == status.HTTP_404_NOT_FOUND
     assert response.json() == {'detail': 'Todo not found'}
     
