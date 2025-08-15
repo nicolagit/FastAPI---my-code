753f835687f8afc3a15c7bea9c49865b4ab76c25
Nicola <nyc.zyp@gmail.com>
2025-08-15 22:27:19 +0100

    15.189 - Full Stack - Register Page

--- TodoApp/routers/auth.py
+++ TodoApp/routers/auth.py
@@ -50,6 +50,10 @@ templates = Jinja2Templates(directory="TodoApp/templates")
 def render_login_page(request: Request):
     return templates.TemplateResponse("login.html", {"request": request})
 
+@router.get("/register-page")
+def render_register_page(request: Request):
+    return templates.TemplateResponse("register.html", {"request": request})
+
 
 ### Endpoints ###
 
