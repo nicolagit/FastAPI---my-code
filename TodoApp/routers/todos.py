77cf9247beab7039d34418dcdb5fe7894066c3c0
Nicola <nyc.zyp@gmail.com>
2025-08-22 19:00:20 +0100

    15.194 - Full Stack - Add New Todo

--- TodoApp/routers/todos.py
+++ TodoApp/routers/todos.py
@@ -60,6 +60,20 @@ async def render_todo_page(request: Request, db: db_dependency):
         return redirect_to_login()
 
 
+@router.get('/add-todo-page')
+async def render_add_todo_page(request: Request, db: db_dependency):
+    try:
+        user = await get_current_user(request.cookies.get("access_token"))
+
+        if user is None:
+            return redirect_to_login()
+
+        return templates.TemplateResponse("add-todo.html", {"request": request, "user": user})
+
+    except:
+        return redirect_to_login()
+
+
 ### Endpoints ###
 
 @router.get("/", status_code=status.HTTP_200_OK)
