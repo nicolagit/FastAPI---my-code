b2cdb196b8d3fb46495688a52edc01c2abff6644
Nicola <nyc.zyp@gmail.com>
2025-08-24 18:21:48 +0100

    15.197 - Full Stack - Home Page Redirection

--- TodoApp/main.py
+++ TodoApp/main.py
@@ -1,23 +1,21 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, status
 from .models import Base
 from .database import engine
 from .routers import auth, todos, admin, users
-from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
+from fastapi.responses import RedirectResponse
 
 
 app = FastAPI()
 
 Base.metadata.create_all(bind=engine)
 
-template = Jinja2Templates(directory="TodoApp/templates")
-
 app.mount("/static", StaticFiles(directory="TodoApp/static"), name="static")
 
 
 @app.get("/")
 def test(request: Request):
-    return template.TemplateResponse("home.html", {"request": request})
+    return RedirectResponse(url="/todos/todo-page", status_code=status.HTTP_302_FOUND)
 
 
 @app.get("/healthy")
